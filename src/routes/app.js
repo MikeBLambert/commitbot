6b0c6461ff4ca6c7aa586d7d1449b37ee4c10af8
mlamb <mlamb128@gmail.com>
2018-12-27 16:14:17 -0800

    Enable slack events subscription by returning challenge sent by slack

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,7 +1,12 @@
-
 const express = require('express');
 const app = express();
 
-// app.use(express.json());
+app.use(express.json());
+
+app.post('/oauth', (req, res) => {
+  let status = 200;
+  const { challenge } = req.body;
+  res.status(status).send(challenge);
+});
 
 module.exports = app;
