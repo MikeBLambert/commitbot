74d76241eb37080a45bb98260f75961d37628d62
mlamb <mlamb128@gmail.com>
2018-12-28 18:31:28 -0800

    Add initial route for Slack authentication and a registration message.

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,12 +1,28 @@
 const express = require('express');
 const app = express();
+const request = require('request');
+// const octokit = require('@octokit/rest')()
+// const fetch = require('node-fetch');
+require('dotenv').config();
 
 app.use(express.json());
 
-app.post('/oauth', (req, res) => {
+app.post('/commitbot', (req, res) => {
   let status = 200;
   const { challenge } = req.body;
-  res.status(status).send(challenge);
+  if (challenge) return res.status(status).send(challenge);
+
+  var data = {form: {
+    token: process.env.SLACK_AUTH_TOKEN,
+    channel: "#general",
+    text: 'Hi there! :wave: I\'m a special bot designed to help you get over your fear of commitment. Committing on GitHub is super important. Let\'s get started! What is your GitHub username?'
+  }};
+  const message = req.body.event.text;
+  if(message.includes('register')) {
+    request.post('https://slack.com/api/chat.postMessage', data, () => {
+      res.json();
+    })
+  }
 });
 
 module.exports = app;
