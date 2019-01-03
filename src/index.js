8e95ee464a0e4dacb3e615ef3542b3194699b80e
mlamb <mlamb128@gmail.com>
2019-01-03 12:23:18 -0800

    Clean up index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ require('dotenv').config();
 const PORT = 4000;
 const express = require('express');
 const app = express();
-app.use(express.json());
-const forkLoad = require('./routes/app');
+const fork = require('./routes/app');
 
 require('./controller/registration');
+require('./controller/edit');
 
-app.use('/fork', forkLoad);
+app.use(express.json());
+app.use('/fork', fork);
 
 app.listen(PORT, () => {
   console.log('Running on port', PORT);
