b81ddaf22ea80e8d4b18ff3e1b981e61f8010ad5
mlamb <mlamb128@gmail.com>
2019-01-02 20:20:43 -0800

    Wire app.js into and registration.js into index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
-const app = require('./routes/app');
-
+require('dotenv').config();
 const PORT = 4000;
+const express = require('express');
+const app = express();
+app.use(express.json());
+const fork = require('./routes/app');
+
+require('./controller/registration');
+
+app.use('/fork', fork);
 
 app.listen(PORT, () => {
   console.log('Running on port', PORT);
