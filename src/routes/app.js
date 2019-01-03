2bfd8d0091b6922b02d665603cba756e00ac1cea
mlamb <mlamb128@gmail.com>
2019-01-02 20:22:00 -0800

    Add slack and github webhooks to app.js

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,28 +1,48 @@
-const express = require('express');
-const app = express();
-const request = require('request');
-// const octokit = require('@octokit/rest')()
-// const fetch = require('node-fetch');
-require('dotenv').config();
+const router = require('express').Router();
+const octokit = require('@octokit/rest')();
+const Slack = require('slack-node');
+const slack = new Slack();
 
-app.use(express.json());
+slack.setWebhook(
+  'https://hooks.slack.com/services/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'
+);
+const payload = {
+  channel: '#general',
+  username: 'webhookbot',
+  text: 'This is posted to #general and comes from a bot named webhookbot.',
+  icon_emoji: ':ghost:'
+};
 
-app.post('/commitbot', (req, res) => {
-  let status = 200;
-  const { challenge } = req.body;
-  if (challenge) return res.status(status).send(challenge);
 
-  var data = {form: {
-    token: process.env.SLACK_AUTH_TOKEN,
-    channel: "#general",
-    text: 'Hi there! :wave: I\'m a special bot designed to help you get over your fear of commitment. Committing on GitHub is super important. Let\'s get started! What is your GitHub username?'
-  }};
-  const message = req.body.event.text;
-  if(message.includes('register')) {
-    request.post('https://slack.com/api/chat.postMessage', data, () => {
-      res.json();
+const listForkActivity = (forkee, forkedRepo) => () => {
+  octokit.activity
+    .listRepoEvents({
+      owner: forkee,
+      repo: forkedRepo
     })
-  }
-});
+    .then(({ data, headers, status }) => {
+      const commits = data.filter(e => e.payload.commits.length > 0);
+      if(commits.length === 0) {
+        slack.webhook(payload, (err, res) => {});
+        setTimeout(listForkActivity(forkee, forkedRepo), 5000);
+      } else if(commits.length > 0) {
+        return;
+      }
+    });
+};
 
-module.exports = app;
+const gitHubAuthentication = () => {
+  octokit.authenticate({
+    type: 'oauth',
+    key: '11076cacdf559a576421',
+    secret: '5d01c66c370b271f27d5e287fcc72d4a2a5adaef'
+  });
+};
+
+module.exports = router.post('/', async(req, res, next) => {
+  const forkee = req.body.forkee.owner.login;
+  const repo = req.body.repository.name;
+
+  await gitHubAuthentication();
+  await listForkActivity(forkee, forkedRepo)();
+});
