11b6d355149b6a41adccbc75ecfe86be4db7af2e
mlamb <mlamb128@gmail.com>
2019-01-03 12:27:08 -0800

    Make fixture file for bot response messages

---
+++ src/fixtures/botResponses.js
@@ -0,0 +1,10 @@
+const BOT_RESPONSES = {
+  REGISTRATION_NAME: "Hi there! I'm a special bot that's supposed to help you get over your fear of commitment! Let's get started by getting to know each other a bit. What's your name?",
+  REGISTRATION_GITHUB_USERNAME: "Cool, cool...how about your GitHub username? (Please be EXACT. Capitalization matters)",
+  REGISTRATION_FAREWELL: "Boom! That's all I need. We're all set.",
+  EDIT_NAME: "Hi again! It looks like we need to make some edits to your profile. Let's try this again...what's your name?",
+  EDIT_GITHUB_USERNAME: "...and your GitHub username? Don't forget to BE EXACT...capitalization matters!",
+  EDIT_FAREWELL: "Awesome. Thanks for the update!"
+};
+
+module.exports = BOT_RESPONSES;
